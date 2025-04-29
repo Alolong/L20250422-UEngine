eeed41ee836ebd92b8fe955c366b3ee1737b42f9
Alolong <ddten123@gmail.com>
2025-04-29 17:47:54 +0900

    df

--- UEngine/Engine/Engine.h
+++ UEngine/Engine/Engine.h
@@ -1,5 +1,6 @@
 #pragma once
 
+
 class UWorld;
 class UInput;
 
--- UEngine/Engine/Main.cpp
+++ UEngine/Engine/Main.cpp
@@ -1,13 +1,26 @@
 #include "Engine.h"
+#include "SDL3/SDL.h"
+
+
+#pragma comment(lib, "SDL3")
 
 int main()
 {
+	SDL_Window* MyWindow = SDL_CreateWindow("Sample", 640, 480, SDL_WINDOW_OPENGL); // 창의 생성
 
 	UEngine* Engine = new UEngine();
+	
+
 
 	Engine->Initiailze();
 
 	Engine->Run();
+
+
+
+	SDL_DestroyWindow(MyWindow);
+	SDL_Quit();
+
 
 	return 0;
 }
